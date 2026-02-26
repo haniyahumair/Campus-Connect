4b4b02a1689b641578ab40b3e211e6084355523c
Haniyah Umair <haniyah.umair.18@gmail.com>
2026-02-26 17:01:55 +0300

    Added Admin HTML

---
+++ admin/admin.js
@@ -0,0 +1,5 @@
+//ai api will be added in config.js and imported from there 
+//make the sidebar functionality work (what is displayed shoudl match the link in sidebar
+//search and filter functionality works
+//approve reject buttons is the main focus on functionlity -- make those work and connect it with being displayed on the campusconnect websiet (ill help!!)
+// ill add a notification feature on campusconnect so we need to deliver that notification and get back here when a new event is added in the pending/waiting approval queue somehow - through supaabse user and admin ids most likely -- whatever the easiset way is 
