dc94ba9e474807b9c69992e199bc5a7611a2d079
Haniyah Umair <haniyah.umair.18@gmail.com>
2026-03-06 13:53:45 +0300

    Fixed api logic

--- scripts/config/supabase.js
+++ scripts/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./env.js";
+
+const SUPABASE_URL = "https://mvhfqxhgxarfnzbjntef.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12aGZxeGhneGFyZm56YmpudGVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDkyNjIsImV4cCI6MjA4NTUyNTI2Mn0._cYsS4bWBTymDWlmxbecu-Xrh7D7bRknCzIU86rGK3M"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-
